e109775b2e4bcd832c47d6297474fd937ecdfe48
Yerbol1 <120712315+Yerbol1@users.noreply.github.com>
2023-04-24 21:39:52 +0600

    Update group_by_info.sql

--- Procedure/group_by_info.sql
+++ Procedure/group_by_info.sql
@@ -1,24 +1,3 @@
-CREATE OR REPLACE PROCEDURE group_by_info()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-  SELECT "Seller".name, COUNT(*) AS product_count
-  FROM "Seller"
-  JOIN "Product" ON "Seller".id = "Product".seller_id
-  GROUP BY "Seller".name;
-END;
-$$;
-
-
-CALL group_by_info();
-
-
-/* 
-ERROR: ОШИБКА:  в запросе нет назначения для данных результата
-HINT:  Если вам нужно отбросить результаты SELECT, используйте PERFORM.
-CONTEXT:  функция PL/pgSQL group_by_info(), строка 3, оператор SQL-оператор
-*/
-
 
 CREATE OR REPLACE PROCEDURE group_by_info()
 LANGUAGE plpgsql
